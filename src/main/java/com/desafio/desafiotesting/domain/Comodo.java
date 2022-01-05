7e610f69dac654ddd94032b2687a8b67a172cd6f
lassuncao <leonardo.assuncao@mercadolivre.com>
2022-01-05 11:36:57 -0300

    alteracao de validacoes no nome comodo

--- src/main/java/com/desafio/desafiotesting/domain/Comodo.java
+++ src/main/java/com/desafio/desafiotesting/domain/Comodo.java
@@ -1,17 +1,27 @@
 package com.desafio.desafiotesting.domain;
 
+import javax.validation.constraints.*;
 import java.io.Serializable;
 
 public class Comodo implements Serializable {
     private static final long serialVersionUID = 7245102729388518152L;
 
+    /***
+     * campo nome do comodo, retorna as validacoes solicitadas no documento fornecido
+     *  1 - Que o campo nao poder estar vazio
+     *  2 - Que comece com uma letra Maiuscula
+     *  3 - Comprimento maximo de 30 caracteres.
+     * @Autor: Leonardo *----*
+     */
+    @NotNull(message = "O campo nao pode ser nulo")
+    @NotEmpty(message = "O campo nao pode ser vazio")
+    @NotBlank(message = "O campo nao pode ser branco")
+    @Max(value = 30, message="O comprimeto do comodo nao pode exceder 30 caracteres")
+    @Pattern(regexp = "^[A-Z][a-z]+")
     private String nome;
+
     private Double largura;
     private Double comprimento;
-
-    public Comodo(){
-
-    }
 
     public Comodo(String nome, Double largura, Double comprimento) {
         this.nome = nome;
