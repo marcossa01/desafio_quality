9c1e6e6938cbf97ddaab33bdbfab24c89c84db10
lassuncao <leonardo.assuncao@mercadolivre.com>
2022-01-05 11:55:58 -0300

    inclusao de dependencia imports resolvidos

--- src/main/java/com/desafio/desafiotesting/domain/Comodo.java
+++ src/main/java/com/desafio/desafiotesting/domain/Comodo.java
@@ -3,26 +3,11 @@ package com.desafio.desafiotesting.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.*;
-
 @Data
 @AllArgsConstructor
 public class Comodo  {
 
-    /***
-     * campo nome do comodo, retorna as validacoes solicitadas no documento fornecido
-     *  1 - Que o campo nao poder estar vazio
-     *  2 - Que comece com uma letra Maiuscula
-     *  3 - Comprimento maximo de 30 caracteres.
-     * @Autor: Leonardo *----*
-     */
-    @NotNull(message = "O campo nao pode ser nulo")
-    @NotEmpty(message = "O campo nao pode ser vazio")
-    @NotBlank(message = "O campo nao pode ser branco")
-    @Max(value = 30, message="O comprimeto do comodo nao pode exceder 30 caracteres")
-    @Pattern(regexp = "^[A-Z][a-z]+")
     private final String nome;
-
     private final Double largura;
     private final Double comprimento;
 
--- src/main/java/com/desafio/desafiotesting/domain/dto/ComodoDto.java
+++ src/main/java/com/desafio/desafiotesting/domain/dto/ComodoDto.java
@@ -4,7 +4,7 @@ import com.desafio.desafiotesting.domain.Comodo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.io.Serializable;
+import javax.validation.constraints.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,7 +12,20 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class ComodoDto {
 
+    /***
+     * campo nome do comodo, retorna as validacoes solicitadas no documento fornecido
+     *  1 - Que o campo nao poder estar vazio
+     *  2 - Que comece com uma letra Maiuscula
+     *  3 - Comprimento maximo de 30 caracteres.
+     * @Autor: Leonardo *----*
+     */
+    @NotNull(message = "O campo nao pode ser nulo")
+    @NotEmpty(message = "O campo nao pode ser vazio")
+    @NotBlank(message = "O campo nao pode ser branco")
+    @Max(value = 30, message="O comprimeto do comodo nao pode exceder 30 caracteres")
+    @Pattern(regexp = "^[A-Z][a-z]+")
     private final String nome;
+
     private final Double largura;
     private final Double comprimento;
 
