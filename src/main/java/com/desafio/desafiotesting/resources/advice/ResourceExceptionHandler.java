cc608178a87deead0341fbe57ed587b208e90b5d
netto-meli <fernando.netto@mercadolivre.com>
2022-01-05 18:25:16 -0300

    v0.2.6 Rename Handler para Advice (package)

--- src/main/java/com/desafio/desafiotesting/resources/advice/ResourceExceptionHandler.java
+++ src/main/java/com/desafio/desafiotesting/resources/advice/ResourceExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.desafio.desafiotesting.resources.exceptions;
+package com.desafio.desafiotesting.resources.advice;
 
 import com.desafio.desafiotesting.exception.RepositoryException;
 import org.springframework.http.HttpStatus;
