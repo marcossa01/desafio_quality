f8ec40f54ce1d87df34c6f7f7d0d9bcedbcf7083
Felipe Bontempo <felipe.bontempo@mercadolivre.com>
2022-01-06 20:36:58 -0300

    Test - verificaSeTotalM2EstaCerto - Verifique se o total de metros quadrados calculados por propriedade está correto

--- src/test/java/com/desafio/desafiotesting/service/test/ImobiliariaServiceTest.java
+++ src/test/java/com/desafio/desafiotesting/service/test/ImobiliariaServiceTest.java
@@ -1,22 +1,43 @@
 package com.desafio.desafiotesting.service.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import com.desafio.desafiotesting.exception.BusinessException;
+import com.desafio.desafiotesting.domain.Casa;
+import com.desafio.desafiotesting.domain.Comodo;
+import com.desafio.desafiotesting.service.CasaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImobiliariaServiceTest {
-/*
-Verifique se o total de metros quadrados
-calculados por propriedade está correto.
 
-Retorna o cálculo correto do total de
-metros quadrados de uma propriedade.*/
+	@Mock
+	private CasaService service;
+
+	@Spy // mock it partially
+	@InjectMocks
+	private CasaService servicerp = new CasaService();
+
+	@BeforeEach
+	public void beforeEach(){
+		MockitoAnnotations.openMocks(this);
+		this.service = servicerp;
+	}
+	/*
+	Verifique se o total de metros quadrados
+	calculados por propriedade está correto.
+
+	Retorna o cálculo correto do total de
+	metros quadrados de uma propriedade.*/
 	@Test
-	public void verificaSeTotalM2EstaCerto() {}
+	public void verificaSeTotalM2EstaCerto() {
+		Casa casaTeste = retornaCasa();
+		Mockito.when(service.findByNome("Casa1")).thenReturn(casaTeste);
+		String areaCasa = service.getAreaCasa(casaTeste.getNome());
+		Assertions.assertEquals("A área total da casa é de: 55.0 metros.", areaCasa);
+	}
 
 	/*Verifique se o bairro de entrada existe no
 repositório de bairros.
@@ -101,5 +122,14 @@ metros quadrados de um cômodo.
 
 	    //assertion
 	    assertTrue(excecaoEsperada.getMessage().contains("Nao eh permitido registro de anuncio com valor zero"));*/
+	}
+
+	private Casa retornaCasa(){
+		List<Comodo> comodos = new ArrayList<>();
+		comodos.add(new Comodo("Cozinha", 3.0, 5.0));
+		comodos.add(new Comodo("Sala", 3.0, 5.0));
+		comodos.add(new Comodo("Quarto", 5.0, 5.0));
+		Casa casa = new Casa("Casa1", "Liverdade", comodos);
+		return casa;
 	}
 }
